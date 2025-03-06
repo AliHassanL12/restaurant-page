06064140795b0b28643c35700cb407fc2d436088
AliHassanL12 <liaqqahl@gmail.com>
2025-03-06 05:08:42 +0000

    Add content dynamically fix
    Now it's added to the correct div and the correct content is also
    rendered.

---
+++ src/page-load.js
@@ -0,0 +1,13 @@
+import restaurantImage from '../images/restaurant-photo.jpg';
+
+const content = document.querySelector('#content');
+const header = document.createElement('header');
+const image = document.createElement('img');
+image.src = restaurantImage;
+header.textContent = 'A Culinary Gem: Where Every Bite Feels Like Perfection';
+const paragraph = document.createElement('p');
+paragraph.textContent = 'This restaurant is nothing short of amazing! From the moment you step inside, you’re greeted with a warm, inviting atmosphere and a delightful aroma that promises an unforgettable dining experience. The food is simply outstanding—every dish is crafted with fresh, high-quality ingredients and bursting with rich, authentic flavors';
+
+content.appendChild(image);
+content.appendChild(header);
+content.appendChild(paragraph);
