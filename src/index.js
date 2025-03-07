aca8358a7a710f5e7e67c2bc2d28e0594c7fbf67
AliHassanL12 <liaqqahl@gmail.com>
2025-03-07 04:42:57 +0000

    Implement function to load home page on button click
    We also implemented a function that removes all the children of co-
    ntent. Now, this way, we have essentially reloaded that specific
    aspect of the page. I will say however that I don't know whether
    reducing text content to an empty string to remove the children
    elements is the most efficient approach, but I hope it is.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
-import './page-load.js'
+import {loadHomePage} from './page-load.js'
+
+loadHomePage();
+
 
 const buttons = document.querySelectorAll('button');
 
@@ -10,7 +13,17 @@ function loadPage(e) {
     const targetButton = e.target
     switch(targetButton.textContent) {
         case 'Home':
-            console.log('hit home');
+            openHomePage();
             break;
     } 
+}
+
+function openHomePage() {
+    removeContentChildren();
+    loadHomePage();
+}
+
+function removeContentChildren() {
+    const content = document.querySelector('#content');
+    content.textContent = '';
 }
--- src/page-load.js
+++ src/page-load.js
@@ -1,9 +1,13 @@
 import '../css/styles.css'
 
-const content = document.querySelector('#content');
-const header = document.createElement('h1');
-header.textContent = 'A Culinary Gem: Where Every Bite Feels Like Perfection';
-const paragraph = document.createElement('p');
-paragraph.textContent = 'This restaurant is nothing short of amazing! From the moment you step inside, you’re greeted with a warm, inviting atmosphere and a delightful aroma that promises an unforgettable dining experience. The food is simply outstanding—every dish is crafted with fresh, high-quality ingredients and bursting with rich, authentic flavors';
-content.appendChild(header);
-content.appendChild(paragraph);
+
+export function loadHomePage() {
+    const content = document.querySelector('#content');
+    const header = document.createElement('h1');
+    header.textContent = 'A Culinary Gem: Where Every Bite Feels Like Perfection';
+    const paragraph = document.createElement('p');
+    paragraph.textContent = 'This restaurant is nothing short of amazing! From the moment you step inside, you’re greeted with a warm, inviting atmosphere and a delightful aroma that promises an unforgettable dining experience. The food is simply outstanding—every dish is crafted with fresh, high-quality ingredients and bursting with rich, authentic flavors';
+    content.appendChild(header);
+    content.appendChild(paragraph);
+}
+
