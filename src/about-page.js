5296b327d8aa0c32a8e308384c99fe3c7674da80
AliHassanL12 <liaqqahl@gmail.com>
2025-03-07 04:36:07 +0000

    Create and add listeners for other pages accessed through buttons

---
+++ src/about-page.js
@@ -0,0 +1,7 @@
+const content = document.querySelector('#content');
+const header = document.createElement('h1');
+header.textContent = 'Contact Us';
+const paragraph = document.createElement('p');
+paragraph.textContent = 'Here\'s where you can contact us';
+content.appendChild(header);
+content.appendChild(paragraph);
--- src/index.js
+++ src/index.js
@@ -1,2 +1,16 @@
 import './page-load.js'
 
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach((button) => {
+    button.addEventListener('click', loadPage);
+})
+
+function loadPage(e) {
+    const targetButton = e.target
+    switch(targetButton.textContent) {
+        case 'Home':
+            console.log('hit home');
+            break;
+    } 
+}
