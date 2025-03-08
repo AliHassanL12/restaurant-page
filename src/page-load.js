c1affbc2f739d80ce7dda7edf6067cba51ed2aec
AliHassanL12 <liaqqahl@gmail.com>
2025-03-08 06:49:50 +0000

    Create section in home page for opening hours
    Still need to get the hours in but we dynamically created the div
    that will hold it. Not to worried about making it look pretty or
    anything. Just need to add the opening hours content now.

--- src/page-load.js
+++ src/page-load.js
@@ -1,5 +1,4 @@
 
-
 export function loadHomePage() {
     const content = document.querySelector('#content');
     const header = document.createElement('h1');
@@ -8,5 +7,20 @@ export function loadHomePage() {
     paragraph.textContent = 'This restaurant is nothing short of amazing! From the moment you step inside, you’re greeted with a warm, inviting atmosphere and a delightful aroma that promises an unforgettable dining experience. The food is simply outstanding—every dish is crafted with fresh, high-quality ingredients and bursting with rich, authentic flavors';
     content.appendChild(header);
     content.appendChild(paragraph);
+
+    // creating a open hours section
+    const hoursInformationDiv = document.createElement('div');
+    hoursInformationDiv.classList.add('hoursDiv');
+    content.appendChild(hoursInformationDiv);
+
+    // create the open hours header
+    const headerDiv = document.createElement('div');
+    hoursInformationDiv.appendChild(headerDiv);
+    const hoursHeader = document.createElement('h2');
+    hoursHeader.textContent = 'Hours';
+    headerDiv.appendChild(hoursHeader);
+
+    // create open hours content
+    const hoursParagraph = document.createElement('p');
 }
 
