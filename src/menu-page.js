1d0b33af7cf0e1ce385eaf35afb0df1d53094129
AliHassanL12 <liaqqahl@gmail.com>
2025-03-09 22:47:51 +0000

    Add price to create drinks function

--- src/menu-page.js
+++ src/menu-page.js
@@ -4,12 +4,12 @@ export function loadMenuPage() {
 }
 
 function generateDrinks() {
-    createDrink('Mystic Berry Fizz', 'A refreshing blend of wild blueberries, blackberries, and raspberries, infused with a hint of mint and topped with sparkling citrus soda for a crisp, invigorating finish');
-    createDrink('Amber Sunset Chai', 'A rich and creamy spiced chai latte with hints of caramel and toasted vanilla, finished with a sprinkle of cinnamon for a warm, cozy indulgence.');
-    createDrink('Crimson Frost Elixir', 'A tangy fusion of pomegranate, hibiscus, and lemon, shaken with crushed ice and a touch of honey for a perfectly balanced sweet-tart refreshment.');
+    createDrink('Mystic Berry Fizz', 'A refreshing blend of wild blueberries, blackberries, and raspberries, infused with a hint of mint and topped with sparkling citrus soda for a crisp, invigorating finish', '£3');
+    createDrink('Amber Sunset Chai', 'A rich and creamy spiced chai latte with hints of caramel and toasted vanilla, finished with a sprinkle of cinnamon for a warm, cozy indulgence.', '£5');
+    createDrink('Crimson Frost Elixir', 'A tangy fusion of pomegranate, hibiscus, and lemon, shaken with crushed ice and a touch of honey for a perfectly balanced sweet-tart refreshment.', '£3');
 }
 
-function createDrink(drinkName, drinkDescription) {
+function createDrink(drinkName, drinkDescription, dishPrice) {
     const content = document.querySelector('#content');
     const div = document.createElement('div');
     div.classList.add('beverages');
@@ -26,6 +26,11 @@ function createDrink(drinkName, drinkDescription) {
     beverageContent.textContent = drinkDescription;
     headerDiv.appendChild(beverageContent);
 
+    const price = document.createElement('p');
+    price.textContent = dishPrice;
+    price.classList.add('price');
+    headerDiv.appendChild(price);
+
 }
 
 function createMainContent() {
@@ -38,23 +43,3 @@ function createMainContent() {
     content.appendChild(subHeader);
 }
 
-function createHoursSection() {
-    const content = document.querySelector('#content');
-    // creating a open hours section
-    const hoursInformationDiv = document.createElement('div');
-    hoursInformationDiv.classList.add('hoursDiv');
-    content.appendChild(hoursInformationDiv);
-
-    // create the open hours header
-    const headerDiv = document.createElement('div');
-    hoursInformationDiv.appendChild(headerDiv);
-    const hoursHeader = document.createElement('h2');
-    hoursHeader.textContent = 'Hours';
-    headerDiv.appendChild(hoursHeader);
-
-    // create open hours content
-    const hoursParagraph = document.createElement('p');
-    hoursParagraph.classList.add('hoursParagraph');
-    hoursParagraph.textContent = 'Monday: 9:00 - 15:00\nTuesday: 9:00 - 15:00\nWednesday: 9:00 - 16:00\nThursday: 9:00 - 16:00\nFriday: 9:00 - 18:00\nSaturday: Closed\nSunday: Closed';
-    headerDiv.appendChild(hoursParagraph);
-}
