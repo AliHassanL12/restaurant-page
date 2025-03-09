697dadd23459364a813f1725f3a2ad13c616f70c
AliHassanL12 <liaqqahl@gmail.com>
2025-03-09 22:51:05 +0000

    Change createDrink function to a createItem function
    Because we are reusing a lot of that code, it doesnt have to be o-
    nly for creating beverages, but any kind of item, be it a dish, d-
    rink or a side.

--- src/menu-page.js
+++ src/menu-page.js
@@ -4,27 +4,27 @@ export function loadMenuPage() {
 }
 
 function generateDrinks() {
-    createDrink('Mystic Berry Fizz', 'A refreshing blend of wild blueberries, blackberries, and raspberries, infused with a hint of mint and topped with sparkling citrus soda for a crisp, invigorating finish', '£3');
-    createDrink('Amber Sunset Chai', 'A rich and creamy spiced chai latte with hints of caramel and toasted vanilla, finished with a sprinkle of cinnamon for a warm, cozy indulgence.', '£5');
-    createDrink('Crimson Frost Elixir', 'A tangy fusion of pomegranate, hibiscus, and lemon, shaken with crushed ice and a touch of honey for a perfectly balanced sweet-tart refreshment.', '£3');
+    createItem('Mystic Berry Fizz', 'A refreshing blend of wild blueberries, blackberries, and raspberries, infused with a hint of mint and topped with sparkling citrus soda for a crisp, invigorating finish', '£3');
+    createItem('Amber Sunset Chai', 'A rich and creamy spiced chai latte with hints of caramel and toasted vanilla, finished with a sprinkle of cinnamon for a warm, cozy indulgence.', '£5');
+    createItem('Crimson Frost Elixir', 'A tangy fusion of pomegranate, hibiscus, and lemon, shaken with crushed ice and a touch of honey for a perfectly balanced sweet-tart refreshment.', '£3');
 }
 
-function createDrink(drinkName, drinkDescription, dishPrice) {
+function createItem(drinkName, drinkDescription, dishPrice) {
     const content = document.querySelector('#content');
     const div = document.createElement('div');
-    div.classList.add('beverages');
+    div.classList.add('item');
     content.appendChild(div);
 
     const headerDiv = document.createElement('div');
     div.appendChild(headerDiv);
-    const drinkHeader = document.createElement('h3');
-    drinkHeader.textContent = drinkName;
-    headerDiv.appendChild(drinkHeader);
+    const itemHeader = document.createElement('h3');
+    itemHeader.textContent = drinkName;
+    headerDiv.appendChild(itemHeader);
 
-    const beverageContent = document.createElement('p');
-    beverageContent.classList.add('beverageContent');
-    beverageContent.textContent = drinkDescription;
-    headerDiv.appendChild(beverageContent);
+    const itemContent = document.createElement('p');
+    itemContent.classList.add('itemContent');
+    itemContent.textContent = drinkDescription;
+    headerDiv.appendChild(itemContent);
 
     const price = document.createElement('p');
     price.textContent = dishPrice;
