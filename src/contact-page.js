2459b4b64cfe3265dde39dd9c0a69005dcd257e2
AliHassanL12 <liaqqahl@gmail.com>
2025-03-10 04:07:46 +0000

    Start createContact() function for contact us page
    Not complete, just started it

--- src/contact-page.js
+++ src/contact-page.js
@@ -1,9 +1,15 @@
+
 export function loadContactPage() {
     const content = document.querySelector('#content');
     const header = document.createElement('h1');
     header.textContent = 'Contact Us';
-    const paragraph = document.createElement('p');
-    paragraph.textContent = 'Here\'s where you can contact us';
     content.appendChild(header);
-    content.appendChild(paragraph);
 }
+
+function createContact() {
+    const content = document.querySelector('#content');
+    const div = document.createElement('div');
+    div.classList.add('item');
+    content.appendChild(div);
+}
+
