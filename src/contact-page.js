ef3b56baa87d84087da0ac9917783505af3a98cb
AliHassanL12 <liaqqahl@gmail.com>
2025-03-11 01:36:53 +0000

    Finish createContact function and create dynamic contact us page info

--- src/contact-page.js
+++ src/contact-page.js
@@ -4,12 +4,26 @@ export function loadContactPage() {
     const header = document.createElement('h1');
     header.textContent = 'Contact Us';
     content.appendChild(header);
+    createContact('Zayn Calder', 'Manager', 'managerAtPrezt@gmail.com', '043921293219');
+    createContact('Elian Voss', 'Head Chef', 'elian.voss@culinaryhub.com', '04567891234');  
+    createContact('Mira Kellan', 'Marketing Director', 'mira.kellan@tasteworks.com', '03984756218');
 }
 
-function createContact() {
+function createContact(name, position, email, phoneNumber) {
     const content = document.querySelector('#content');
     const div = document.createElement('div');
     div.classList.add('item');
     content.appendChild(div);
+
+    const headerDiv = document.createElement('div');
+    div.appendChild(headerDiv);
+    const itemHeader = document.createElement('h3');
+    itemHeader.textContent = name;
+    headerDiv.appendChild(itemHeader);
+
+    const itemContent = document.createElement('p');
+    itemContent.classList.add('itemContent');
+    itemContent.textContent = `${position}\n${email}\n${phoneNumber}`;
+    headerDiv.appendChild(itemContent);
 }
 
